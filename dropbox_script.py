3b24100f4e18a7645191037368d8d66bc5fc54fe
Alphawarrior21 <sumitchandmale@gmail.com>
2024-05-26 15:03:47 +0530

    Aerointel intial commit with git config

--- dropbox_script.py
+++ dropbox_script.py
@@ -3,14 +3,14 @@ import os
 
 def download_files():
     # Define your access token
-    ACCESS_TOKEN = 'sl.B1nX501NKRjohwMJmE04PeAFhu7KP3Q2AaI1_BevZVVKYjaDOwUyTXrS-CYtVQMSMddMP5XWOFA7nyCetCIprRMHlgvu9sKD3ROPONQmwjs1AUNp2ww7I63l_N1zzvw-nxaTRPLq1LMUtqm4-yo7EOQ'
+    ACCESS_TOKEN = 'sl.B15-H2CsWk4Xiio4qNbO2b_wnH3l0q7bv4OuumD4bbEm7rLBbXe2Y04man8aZaIo8SRAHGL7q_MS4_ZWs4xO5XSVk2ktsZAy4g2t5TgtUsqVtHoKDvCzJ69c8NSc0-OsuHyB413SxQTeOYRxh7kb'
 
     # Initialize Dropbox object
     dbx = dropbox.Dropbox(ACCESS_TOKEN)
 
     # Specify the Dropbox path and local folder path
     dropbox_path = '/RagBasedLLM'
-    local_path = '/home/sumit/gate_cse_gpt/Dropbox'
+    local_path = '/home/sumit/AeroIntel/Dropbox'
 
     # Create local folder if it doesn't exist
     if not os.path.exists(local_path):
